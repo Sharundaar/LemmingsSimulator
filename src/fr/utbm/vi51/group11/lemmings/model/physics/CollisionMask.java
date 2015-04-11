e847e9ed86225f40c71d0b9daef91d06fd9e1363
Donatien Rabiller <rabiller.donatien@hotmail.fr>
2015-04-11 19:26:22 +0200

    Started implementing CollisionMask/Shape logic

--- src/fr/utbm/vi51/group11/lemmings/model/physics/CollisionMask.java
+++ src/fr/utbm/vi51/group11/lemmings/model/physics/CollisionMask.java
@@ -1,11 +1,16 @@
 package fr.utbm.vi51.group11.lemmings.model.physics;
 
+import java.util.LinkedList;
+
 import org.arakhne.afc.math.continous.object2d.Point2f;
 
 public class CollisionMask
 {
 	/** Absolute coordinates */
 	private Point2f	m_coordinates;
+	
+	/** List of sub-shapes */
+	private LinkedList<CollisionShape> m_shapes = new LinkedList<CollisionShape>();
 
 	public CollisionMask()
 	{
@@ -19,5 +24,10 @@ public class CollisionMask
 	public Point2f getCoordinates()
 	{
 		return m_coordinates;
+	}
+	
+	public LinkedList<CollisionShape> getShapes()
+	{
+		return m_shapes;
 	}
 }
