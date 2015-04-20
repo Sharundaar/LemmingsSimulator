313eed73c33bc974b0d6a7dc81f861276f53a07c
Donatien Rabiller <rabiller.donatien@hotmail.fr>
2015-04-20 13:34:05 +0200

    CollisionMask: collide func is implemented but need tests

--- src/fr/utbm/vi51/group11/lemmings/model/physics/CollisionMask.java
+++ src/fr/utbm/vi51/group11/lemmings/model/physics/CollisionMask.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import org.arakhne.afc.math.continous.object2d.Point2f;
 
 public class CollisionMask extends CollisionShape
-{
+{	
 	public CollisionMask()
 	{
 		super(CollisionShapeType.MASK, null);
@@ -26,7 +26,52 @@ public class CollisionMask extends CollisionShape
 	@Override
 	public boolean collide(CollisionShape _shape) {
 		/** TODO: implementation */
+		
+		for(CollisionShape shape : m_childs)
+		{
+			if(collideRec(shape, _shape))
+			{
+				return true;
+			}
+			else
+				continue;
+		}
+		
 		return false;
+	}
+	
+	private boolean collideRec(CollisionShape _s1, CollisionShape _s2)
+	{
+		if(!_s1.collide(_s2))
+		{
+			return false;
+		}
+		else
+		{
+			for(CollisionShape shape : _s2.getChilds()) // here we go down one level on _s2
+			{
+				if(!_s1.collide(shape))
+					continue;
+				else
+				{
+					return collideRec(shape, _s1); // invert the parameters here so we go down one level on _s1
+				}
+			}
+			
+			// If we arrive here it means that _s2 has no childs but maybe _s1 has
+			for(CollisionShape shape : _s1.getChilds())
+			{
+				if(!shape.collide(_s2)) // shape doesn't collide _s2
+					continue;
+				else
+				{
+					return collideRec(_s2, shape); // s2 has no child so we want to go deeper in shape (no inuendo intended)
+				}
+			}
+			
+			// If we arrive here then s1 collide with s2, and s1/s2 are leafs so the conclusion should be true
+			return true;
+		}
 	}
 
 	@Override
